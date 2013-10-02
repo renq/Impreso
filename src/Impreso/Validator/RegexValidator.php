d27647f7fcaa7a99fcdec5db29a5516d45745741
Michał Lipek <m.lipek@goldensubmarine.com>
2013-10-02 17:24:20 +0200

    Regex validator

---
+++ src/Impreso/Validator/RegexValidator.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Michał Lipek
+ * Date: 02.10.13
+ * Time: 16:30
+ */
+
+namespace Impreso\Validator;
+
+
+class RegexValidator extends Validator
+{
+
+    private $regex;
+
+    public function __construct($error, $regex = null)
+    {
+        $this->setRegex($regex);
+    }
+
+    public function setRegex($regex)
+    {
+        $this->regex = $regex;
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function validate($value)
+    {
+        $result = @preg_match($this->regex, $value);
+        if ($result === false) {
+            throw new \InvalidArgumentException("Incorrect regular expression: $this->regex");
+        }
+        return (bool)$result;
+    }
+}
---
+++ tests/Impreso/Validator/RegexValidatorTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Michał Lipek
+ * Date: 02.10.13
+ * Time: 17:01
+ */
+
+namespace Tests\Impreso\Helper;
+
+
+use Impreso\Validator\RegexValidator;
+
+class RegexValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testStandardUsage()
+    {
+        $v = new RegexValidator('error');
+        $v->setRegex('/^[0-1]+$/');
+        $this->assertTrue($v->validate('0110101011101110'));
+        $this->assertFalse($v->validate('abc'));
+    }
+
+    public function testIncorrectRegex()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $v = new RegexValidator('error', '/[+/');
+        $v->validate('a');
+    }
+}
